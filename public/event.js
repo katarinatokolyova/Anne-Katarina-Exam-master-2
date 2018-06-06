085ec67ef5c9df086f2f4968b1cb58b336a6a006
Katarina Tokolyova <katarina.tokolyova@gmail.com>
2018-06-06 16:47:11 +0200

    More adjustments

--- public/event.js
+++ public/event.js
@@ -1,37 +1,38 @@
+
 var Eventplatform = (function () {
 
     // Local array to keep track of data
     let comments = []
-    let guestList = []
-
-
+    
+    
     // Elements
     const eventWindowElement = document.querySelector('#event-window')
     const eventCommentElement = document.querySelector('#comment-window')
 
+    
     // Templates
     const userTemplate = ({ username }) => `
     <li class="media my-2">
-    <div class="media-body">
+      <div class="media-body">
         <b class="text-secondary">${username}</b>
-    </div>
+      </div>
     </li>
-`
-
+    `
+    
     const commentTemplate = ({text, user}) => `
     <div class="media my-3 chat-message">
-    <div class="media-body">
+      <div class="media-body">
         <div class="mt-0"><b>${user.username}</b></div>
         <p>
-        ${text}
+          ${text}
         </p>
+      </div>
     </div>
-    </div>
-`
-
+    `
+    
     // Private frontend module
     let frontend = {}
-
+    
     frontend.autoScroll = function () {
         let eW = eventCommentElement
         
@@ -39,61 +40,61 @@ var Eventplatform = (function () {
             eW.scrollTop += 150
         }
     }
-
+    
     frontend.addComment = function (comment) {
         let html = commentTemplate(comment)
-
+    
         eventCommentElement.insertAdjacentElement('beforeend', html)
-
+    
         frontend.autoScroll ()
     }
-
+    
     frontend.addUser = function (user){
         let html = guestListTemplate (user)
-
+    
         guestListElement.insertAdjacentElement('beforeend', html)
         frontend.autoScroll ()
     }
-
+    
     frontend.getCommentHTML = function (comments) {
         let html = ''
-
+    
         comments.forEach (e => {
             html += commentTemplate(e)
         })
         return html
     }
-
+    
     frontend.displayComments = function (comments) {
         eventWindowElement.innerHTML.getCommentHTML(events)
-
+    
     }
-
+    
     //Initializing and empty object 
     let module = {}
-
+    
     module.sendComment = function (comment) {
         comment.push(comment)
         frontend.addComment(comment)
     }
-
+    
     //Return the comment array
     module.getComment = function () {
         return comments
     }
-
+    
     return module
-
+    
     function EventComment (comment, user)
         this.comment = comment
         this.user = user
         this.createdAt = new Date()
     })
-
-
-    //method for writing a comment
+    
+    
+    //method for writing a comment Testing the comment method 
     module.sendComment2 = function (comment) {
-        console.log("inde i sendcomment")
+        console.log("inde i send comment")
         // // The "credentials" option makes sure that the browser's cookies
         // are sent back and forth during a request
         // Please refer to the fetch() docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
@@ -116,7 +117,7 @@ var Eventplatform = (function () {
             }    
         }) 
     } 
-
+    
     //returns the comment array - server
     module.getComment = function (event) {
         // The credentials option makes sure that the browsers cookies are sent back and forth during a request
@@ -126,83 +127,80 @@ var Eventplatform = (function () {
         return fetch('/api/comments', options)
         .then(response => response.json())
     }
-
-
+    
+    
     //module.getComments (EVENT_ID)
     module.getComment = function (EVENT_ID) {
         // The credentials option makes sure that the browsers cookies are sent back and forth during a request
         let options = {
             credentials: 'include'
         }
-
+    
         return fetch(`/api/events/${event_id}/comments, options`)
         .then(response => response.json())
     }
-
+    
     // Function which sets up listeners for Socket.io events 
     // And loads initial comments to the commen window
     function initialize() {
         // Setup the socket 
         let socket = io() 
-
+    
     // What to do when the "new comment" event is received
     socket.on('new comment', comment => {
         // Add the message to the chat window
         frontend.addComment(comment)
     })
-
+    
     // Load all comments and display them
     module.getComment()
     .then(allComments => {
         comments = allComments
-
+    
         frontend.displayComments(comments)
     })
     }
-    
-    //Connecting the project to an external API
+
     module.getRandomPicture = function () {
         let url = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1`;
         let options = {
-        method: 'get',
-        headers: {
+          method: 'get',
+          headers: {
             "X-Mashape-Key": "8JjZG4xR9rmsh4rky1POhsezdRkQp1faNEdjsndxjV8KBtLS7a",
             "content-type": 'application/json',
-        }
+          }
         }
         fetch(url, options)
-        .then(response => response.json())
-        .then(results => {
+          .then(response => response.json())
+          .then(results => {
             console.log(results.recipes[0].image) // This contains an URL to a random image of a random meal
+      
+          })
+          }
+      
     
-        })
-        }
-        
     // Run the initialize function 
     initialize()
-
+    
     return module
-
+    
     //Adding comment to event
     function EventComment (text) {
         this.text = text
     }
-
+    
     document.querySelector('#comment-form').addEventListener('submit', event => {
         event.preventDefault()
-
+    
         let input = document.querySelector('#EventComment')
-
+    
         let text = input.value
         let newCommentObject = new EventComment(text)
-
+    
         Comment.sendComment(newCommentobject)
-
+    
         input.value = ''
-
-
-
-})
-
-
-
+    
+    
+    
+    })
